545c88bea2ca3bb6db19cf0fa93dc168483cadba
admin-pc <379216188@qq.com>
2017-10-19 22:54:16 +0800

    20171019日常备份

--- data/config.cache.inc.php
+++ data/config.cache.inc.php
@@ -1,7 +1,7 @@
 <?php
 $cfg_disable_funs = 'phpinfo,eval,exec,passthru,shell_exec,system,proc_open,popen,curl_exec,curl_multi_exec,parse_ini_file,show_source,file_put_contents';
 $cfg_disable_tags = 'php';
-$cfg_basehost = 'http://lh.1314theone.com';
+$cfg_basehost = 'http://1314theone.me';
 $cfg_cmspath = '';
 $cfg_cookie_encode = 'KWHR7RvDPlmbm8iwRVSGXBqkQADr47';
 $cfg_indexurl = '/';
--- static/js/index.js
+++ static/js/index.js
@@ -7,7 +7,7 @@ $(function(){
 		}
 		$(this).find(".circle").show();
 	},function(){
-		$(this).find(".sidebox").stop().animate({"width":"54px"},200).css({"opacity":"0.8","filter":"Alpha(opacity=80)","background":"#7DBEFF"})
+		$(this).find(".sidebox").stop().animate({"width":"54px"},200).css({"opacity":"0.8","filter":"Alpha(opacity=80)","background":"#7DBEFF"});
 		$(this).find(".circle").hide();	
 	});
 });
@@ -25,13 +25,13 @@ function goTop(){
 }
 $(function(){
 	var a = $('.header-frame'),
-		b = a.offset();
+		b = a.offset();	//获取匹配元素(a)在当前视口的相对偏移，包括top和left。
 	$(document).on('scroll',function(){
-		var	c = $(document).scrollTop();
+		var	c = $(document).scrollTop();	//获取匹配元素相对滚动条顶部的偏移
 		if(b.top<=c){
-			a.css({'position':'fixed','top':'0px'})
+			a.css({'position':'fixed','top':'0px'});
 		}else{
-			a.css({'position':'absolute','top':'0px'})
+			a.css({'position':'relative','top':'0px'});
 		}
 	});
 });
